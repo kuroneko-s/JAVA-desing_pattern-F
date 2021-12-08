a218885035b83aea5837871db5fce552cb9f12cb
Deployer <drivespublic@gmail.com>
2021-12-08 14:26:58 +0900

    Memento pattern

--- src/main/java/com/inflearn/desing_pattern/memento/MementoRunner.java
+++ src/main/java/com/inflearn/desing_pattern/memento/MementoRunner.java
@@ -4,8 +4,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.Date;
 import java.util.Map;
 import java.util.Set;
@@ -32,9 +31,31 @@ public class MementoRunner implements ApplicationRunner {
         Date date = new Date();
         date.getTime();
 
-        try(FileOutputStream fileOutputStream = new FileOutputStream("gamesave.hex");
+        try(FileOutputStream fileOutputStream = new FileOutputStream("game.txt");
             ObjectOutputStream out = new ObjectOutputStream(fileOutputStream)){
-            out.writeObject(careTake);
+            out.writeObject(game.save());
         }
+        // 역직렬화는 쓰면 위험하다 ( 이펙티브 자바 )
+        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream("game.txt"))){
+            in.setObjectInputFilter(createFilter());
+            in.readObject();
+            // System.out.println(game1.toString());
+        }catch (InvalidClassException e) {
+            System.out.println(e);
+        }
+        // https://madplay.github.io/post/prefer-alternatives-to-java-serialization
+        // https://madplay.github.io/post/why-java-serialization-is-bad#%EC%97%AD%EC%A7%81%EB%A0%AC%ED%99%94-%ED%95%84%ED%84%B0%EB%A7%81
+    }
+
+    private static ObjectInputFilter createFilter() {
+        return filterInfo -> {
+            Class<?> aClass = filterInfo.serialClass();
+            System.out.println("serialClass - " + aClass.toString());
+            if (Game.class.isAssignableFrom(aClass)) {
+                return ObjectInputFilter.Status.ALLOWED;
+            }
+            System.err.println("Rejected : " + aClass.getSimpleName());
+            return ObjectInputFilter.Status.REJECTED;
+        };
     }
 }
